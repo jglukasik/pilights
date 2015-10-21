7357ff78ed21fb7f54000d6d0195f499788ea979
Joseph Lukasik <jglukasik@wisc.edu>
2015-10-21 06:25:07 +0000

    initial commit

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script installs the necessary dependencies for the raspberry pi wx281x
+# library for powering led light strips.
+
+sudo apt-get update && apt-get install -y \
+  vim \
+  tmux \
+  mosh \
+  build-essential \
+  python-dev \
+  git \
+  scons \
+  swig \
+
+git clone https://github.com/jgarff/rpi_wx281x.git
+cd rpi_ws281x
+scons
+cd python
+sudo python setup.py install
+
+
