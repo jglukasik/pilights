205ef38c1324c62231c851066234a4d7699a0873
Joseph Lukasik <jglukasik@wisc.edu>
2015-10-29 03:20:57 +0000

    On, off, pulsing scripts

---
+++ ambient.py
@@ -0,0 +1,38 @@
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+#
+# jglukasik - making some changes of my own to learn this api
+
+import time
+
+from neopixel import *
+
+
+# LED strip configuration:
+LED_COUNT      = 150      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+
+def breathe(wait_ms=10000):
+  for i in range(20, 60):
+    for j in range(0, strip.numPixels()):
+      strip.setPixelColorRGB(j,255,255,255)
+    strip.setBrightness(i)
+    strip.show()
+    time.sleep(wait_ms/1000.0)
+
+# Main program logic follows:
+if __name__ == '__main__':
+  # Create NeoPixel object with appropriate configuration.
+  strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+  # Intialize the library (must be called once before other functions).
+  strip.begin()
+  
+  breathe();
