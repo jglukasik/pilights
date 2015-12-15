dccc51a08f69e493a9ce117e9984da7990a55d4e
Joseph Lukasik <jglukasik@wisc.edu>
2015-12-15 07:14:12 +0000

    Adding init script

--- install.sh
+++ install.sh
@@ -3,6 +3,19 @@ set -e
 
 DIR="/home/pi/pilights/"
 
-# This script installs the pilight application and webserver
+# Install websocket application
 cp "$DIR/bin/pilights.py" /usr/sbin/pilights
+
+# Install web server
 cp -r "$DIR/www/" /var/www/pilights
+
+# Install init script
+cp "$DIR/etc/pilights" /etc/init.d/pilights
+
+ln -sf /etc/init.d/pilights /etc/rc0.d/K01pilights
+ln -sf /etc/init.d/pilights /etc/rc1.d/K01pilights
+ln -sf /etc/init.d/pilights /etc/rc6.d/K01pilights
+
+ln -sf /etc/init.d/pilights /etc/rc3.d/S03pilights
+ln -sf /etc/init.d/pilights /etc/rc4.d/S03pilights
+ln -sf /etc/init.d/pilights /etc/rc5.d/S03pilights
